f4b1d71f24f6f20a5400d1c40ec3143952db18bc
sage-etcher <sage.message@email.com>
2025-02-06 21:20:38 +0000

    create a window, do nothing more

--- src/main_window.c
+++ src/main_window.c
@@ -5,19 +5,29 @@
 static void
 app_activate (GApplication *app, gpointer *user_data)
 {
-    g_print ("GtkApplication is activated.\n");
+    const char *WIN_TITLE = "Northstar Advantage Emulator";
+    const int WIN_WIDTH   = 400;
+    const int WIN_HEIGHT  = 300;
+
+    GtkWidget *win = NULL;
+
+    win = gtk_application_window_new (GTK_APPLICATION (app));
+    gtk_window_set_title (GTK_WINDOW (win), WIN_TITLE);
+    gtk_window_set_default_size (GTK_WINDOW (win), WIN_WIDTH, WIN_HEIGHT);
+
+    gtk_window_present (GTK_WINDOW (win));
 }
 
 
 int
 main (int argc, char **argv)
 {
+    const char *APPLICATION_ID = "org.sageetcher.NorthstarAdvantageEmulator";
+
     int status = 0;
     GtkApplication *app = NULL;
 
-    app = gtk_application_new ("org.sageetcher.NorthStarAdvantageEmulator", 
-                               G_APPLICATION_DEFAULT_FLAGS);
-
+    app = gtk_application_new (APPLICATION_ID, G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv); 
 
